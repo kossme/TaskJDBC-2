fc34522cdd74aef702d3c90cc34e9e3c83538129
Konstantin <kossme@mail.ru>
2020-09-22 15:53:12 +0300

    2

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    private UserDao userDao = new UserDaoHibernateImpl();
+    private UserDao userDao = new UserDaoHibernateImpl(); //Hibernate implementation
 
     public void createUsersTable()  {
         userDao.createUsersTable();
